de4bd6108753782478a8e120d1e1320ce1c419d9
Nahom Abera <nahomtesfahun001@gmail.com>
2024-04-25 11:43:12 -0400

    Modify Main.java and AddSsnColumnInDatabase.java

--- src/AddSsnColumnInDatabase.java
+++ src/AddSsnColumnInDatabase.java
@@ -7,21 +7,24 @@ public class AddSsnColumnInDatabase {
         this.employeeDatabase = employeeDatabase;
     }
 
-    void addSSNColumn() {
-        try {
-            DatabaseMetaData dbmd = employeeDatabase.connection.getMetaData();
-            ResultSet rs = dbmd.getColumns(null, null, "employees", "SSN");
-            if (!rs.next()) {
-                String query = "ALTER TABLE employees ADD COLUMN SSN VARCHAR(9)";
-                try (PreparedStatement pstmt = employeeDatabase.connection.prepareStatement(query)) {
-                    employeeDatabase.executeUpdate(pstmt);
-                    System.out.println("SSN column added successfully.");
-                }
-            } else {
-                System.out.println("SSN column already exists.");
+    void addSSNColumn() throws SQLException {
+        if (employeeDatabase.isSSNColumnAvailable()){
+            System.out.println("SSN Column Already Exists in the Database.");
+        }
+        else{
+            try {
+                DatabaseMetaData dbmd = employeeDatabase.connection.getMetaData();
+                ResultSet rs = dbmd.getColumns(null, null, "employees", "SSN");
+                if (!rs.next()) {
+                    String query = "ALTER TABLE employees ADD COLUMN SSN VARCHAR(9)";
+                    try (PreparedStatement pstmt = employeeDatabase.connection.prepareStatement(query)) {
+                        employeeDatabase.executeUpdate(pstmt);
+                        System.out.println("SSN column added successfully. \nSelect \"4. Update Employee Data\" on Main Window to add the employees SSN. ");
+                    }
+                } 
+            } catch (SQLException e) {
+                System.out.println("Failed to add SSN column: " + e.getMessage());
             }
-        } catch (SQLException e) {
-            System.out.println("Failed to add SSN column: " + e.getMessage());
         }
     }
 }
