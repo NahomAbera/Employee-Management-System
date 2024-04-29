d60cf6ffdaf941730efc518570ba5da2a206400c
Nahom Tesfahun Abera <144270821+NahomAbera@users.noreply.github.com>
2024-04-29 17:32:30 -0400

    Add Screen Clearing Functionality To Main Window

--- src/Main.java
+++ src/Main.java
@@ -37,6 +37,8 @@ public class Main {
                 System.out.print("Enter your choice: ");
                 choice = scanner.nextInt();
                 scanner.nextLine(); 
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
 
                 switch (choice) {
                     case 1:
